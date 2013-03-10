a917d0292729b3104d9b034a25b09d230d530b60
grrrwaaa <grrrwaaa@gmail.com>
2013-03-10 14:30:15 +0900

    reorganized

---
+++ src/makeluajitosx.sh
@@ -0,0 +1,35 @@
+# grab source:
+pushd ..
+git submodule init && git submodule update
+pushd luajit-2.0
+
+# build 32-bit
+make clean
+make CC="gcc -m32"
+sudo make install
+cp /usr/local/lib/libluajit-5.1.a ../src/osx/lib/libluajit32.a
+
+# copy headers:
+cp /usr/local/include/luajit-2.0/* ../src/osx/include/
+
+# build 64-bit
+make clean
+make CC="gcc -m64"
+make 
+sudo make install
+cp /usr/local/lib/libluajit-5.1.a ../src/osx/lib/libluajit64.a
+
+# restore normality
+make clean
+make
+sudo make install
+
+popd
+popd
+
+# create FAT binary:
+pushd osx/lib
+lipo -create libluajit32.a libluajit64.a -output libluajit.a
+rm libluajit64.a
+rm libluajit32.a
+popd
