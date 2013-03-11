13c4306b7ecb5ce5c7479593106f61c93698178e
grrrwaaa <grrrwaaa@gmail.com>
2013-03-11 13:11:37 +0900

    added missing modules

---
+++ modules/field2D.lua
@@ -0,0 +1,177 @@
+local ffi = require "ffi"
+local gl = require "gl"
+local sketch = gl.sketch
+
+local floor = math.floor
+
+local field2D = {}
+field2D.__index = field2D
+
+function field2D:reduce(func, result)
+	for y = 0, self.height-1 do
+		for x = 0, self.width-1 do
+			result = func(result, self.data[self:index_raw(x, y)], x, y)
+		end
+	end
+	return result
+end
+
+-- e.g.:
+function field2D:sum()
+	return self:reduce(function(total, cell)
+		return total + cell
+	end, 0)
+end
+
+-- field:find(predicate), to return a list of coordinates usable by a range map?
+-- gets closer to a jquery style of select -> apply (find -> map)
+
+-- field:map(func)
+-- field:map(func, 10, 10)
+-- does it mean range 2..4 or only use indices 2,4 ? 
+-- field:map(func, { 2, 4 }, { 4, 6 })
+-- field:map(func, iterator)
+--[[
+function field2D:map(range, func)
+	if func == nil then
+		-- no range given, apply to whole field:
+		return self:map(self, range)
+	
+	if type(range) == "function" then
+		-- keep calling range() until nil:
+		for x, y in range() do
+			self:set(f(x, y), x, y)
+		end
+	else
+		-- assume object:
+		
+	end
+end
+--]]
+function field2D:index(x, y)
+	x = floor(x and (x % self.width) or 0)
+	y = floor(y and (y % self.height) or 0)
+	return y*self.width + x
+end
+
+function field2D:index_raw(x, y)
+	return y*self.width + x
+end
+
+function field2D:set(v, x, y)
+	if x then
+		local idx = self:index(x, y or 0)
+		self.data[idx] = (type(v) == "function" and v(x, y)) or (v and tonumber(v)) or 0
+		return self
+	elseif type(v) == "function" then
+		for y = 0, self.height-1 do
+			for x = 0, self.width-1 do
+				local idx = self:index_raw(x, y)
+				local result = v(x, y)
+				if result then
+					self.data[idx] = result
+				end	
+			end
+		end
+	else
+		v = v and tonumber(v) or 0
+		for y = 0, self.height-1 do
+			for x = 0, self.width-1 do
+				local idx = self:index_raw(x, y)
+				self.data[idx] = v
+			end
+		end
+	end
+	return self
+end
+
+function field2D:get(x, y)
+	return self.data[self:index(x, y)]
+end
+
+function field2D:clear()
+	ffi.fill(self.data, self.size)
+end
+
+function field2D:map(func)
+	for y = 0, self.height-1 do
+		for x = 0, self.width-1 do
+			local idx = self:index_raw(x, y)
+			local v = func(self.data[idx], x, y)
+			if v then
+				self.data[idx] = v
+				--print(x, y, idx, self.data[idx])
+			end	
+		end
+	end
+end
+
+-- NOTE: this also leaves the texture bound
+function field2D:send(unit)
+	self:bind(unit)
+	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.LUMINANCE, self.width, self.height, 0, gl.LUMINANCE, gl.FLOAT, self.data)
+end
+
+-- NOTE: this also leaves the texture bound
+function field2D:draw(x, y, w, h, unit)
+	self:send(unit)
+	sketch.quad(x or 0, y or 0, w or 1, h or 1)
+end
+
+function field2D:create()
+	-- turn this one even if we already created it.
+	gl.Enable(gl.TEXTURE_2D)
+		
+	if not self.texID then
+		self.texID = gl.GenTextures(1)
+		gl.BindTexture(gl.TEXTURE_2D, self.texID)
+		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP)
+		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP)
+		gl.TexImage2D(gl.TEXTURE_2D, 0, gl.LUMINANCE, self.width, self.height, 0, gl.LUMINANCE, gl.FLOAT, self.data)
+		gl.BindTexture(gl.TEXTURE_2D, 0)		
+	end
+end
+
+function field2D:bind(unit)
+	--gl.ActiveTexture(gl.TEXTURE0 + (unit or 0))
+	self:create()
+	
+	gl.BindTexture(gl.TEXTURE_2D, self.texID)
+end
+
+function field2D:unbind(unit)
+	--gl.ActiveTexture(gl.TEXTURE0 + (unit or 0))
+	gl.BindTexture(gl.TEXTURE_2D, 0)
+end
+
+function field2D:copy()
+	local f2 = field2D.new(self.width, self.height)
+	-- copy data:
+	ffi.copy(self.data, f2.data, f2.size)
+	return f2
+end
+
+function field2D.new(dimx, dimy)
+	dimx = dimx or 64
+	dimy = dimy or dimx
+	local data = ffi.new("float[?]", dimx*dimy)
+	
+	return setmetatable({
+		data = data,
+		-- dimensions:
+		dim = { dimx, dimy },
+		-- human-readable...
+		width = dimx,
+		height = dimy,
+		-- size in bytes:
+		size = ffi.sizeof(data),
+	}, field2D)
+end
+
+return setmetatable(field2D, {
+	__call = function(_, ...)
+		return field2D.new(...)
+	end,
+})
---
+++ modules/window.lua
@@ -0,0 +1,207 @@
+local ffi = require "ffi"
+local lib = ffi.C
+
+local gl = require "gl"
+
+local Window = {}
+
+function Window:__tostring()
+	return string.format("Window(%p)", self)
+end
+
+local key_events = {
+	"down",
+	"up"
+}
+
+local mouse_events = {
+	[0] = "down",
+	"up",
+	"drag",
+	"move",
+}
+
+local t = lib.av_time()
+
+function Window:__newindex(k, v)
+	if k == "key" then
+		--[[
+		self.onkey:set(function(self, e, k)
+			e = key_events[e]
+			
+			-- built-in keys:
+			if e == "down" and k == 27 then
+				-- fullscreen flip:
+				self.fullscreen = not self.fullscreen
+			end
+		
+			local ok, err = pcall(v, self, e, k)
+			if not ok then print(debug.traceback(err)) end
+		end)
+		--]]
+	elseif k == "draw" then
+		--[[
+		self.ondraw:set(function(self)
+			
+			collectgarbage()
+			
+			local t1 = lib.av_time()
+			dt = t1 - t
+			t = t1
+			
+			local w, h = self.width, self.height
+			
+			-- set up 2D mode by default:
+			gl.Viewport(0, 0, w, h)
+			gl.MatrixMode(lib.GL_PROJECTION)
+			gl.LoadIdentity()
+			gl.Ortho(0, w, h, 0, -100, 100)
+			gl.MatrixMode(lib.GL_MODELVIEW)
+			gl.LoadIdentity()
+
+			local ok, err = pcall(v, self, w, h, dt)
+			if not ok then print(debug.traceback(err)) end	
+		end)
+		--]]
+	elseif k == "create" then
+		self.oncreate:set(function(self)
+			local ok, err = pcall(v, self)
+			if not ok then print(debug.traceback(err)) end
+		end)
+		
+	elseif k == "resize" then
+		self.onresize:set(function(self, w, h)
+			print("resize", w, h)
+			local ok, err = pcall(v, self, w, h)
+			if not ok then print(debug.traceback(err)) end
+		end)
+		
+	elseif k == "visible" then
+		self.onvisible:set(function(self, s)
+			local ok, err = pcall(v, self, s)
+			if not ok then print(debug.traceback(err)) end
+		end)
+		
+	elseif k == "mouse" then
+		--[[
+		self.onmouse:set(function(self, e, b, x, y)
+			local ok, err = pcall(v, self, mouse_events[e], b, x, self.height-y-1)
+			if not ok then print(debug.traceback(err)) end
+		end)
+		--]]
+	elseif k == "fullscreen" then
+		self:setfullscreen(v)
+		
+	--[[
+	elseif k == "title" then
+		self:settitle(v)
+		
+	elseif k == "dim" then
+		self:setdim(unpack(v))
+	--]]	
+	else
+		error("cannot assign to Window: "..k)
+	end
+end
+
+function Window:__index(k)
+	if k == "fullscreen" then
+		return self.is_fullscreen ~= 0
+	elseif k == "dim" then
+		return { self.width, self.height }
+	else
+		return Window[k]
+	end
+end
+
+setmetatable(Window, {
+	__index = function(self, k)
+		Window[k] = lib["av_window_" .. k]
+		return Window[k]
+	end,
+})
+
+ffi.metatype("av_Window", Window)
+
+local win = lib.av_window_create()
+local updating = true
+
+-- set default callbacks:
+win.ondraw = function(self) 
+	collectgarbage()
+			
+	local t1 = lib.av_time()
+	dt = t1 - t
+	t = t1
+	
+	if updating and update and type(update) == "function" then
+		local ok, err = pcall(update, dt)
+		if not ok then 
+			print(debug.traceback(err)) 
+			-- prevent error spew:
+			update = nil
+		end	
+	end
+
+	local w, h = self.width, self.height
+	
+	-- set up 2D mode by default
+	-- (should we use 0..1 instead?)
+	gl.Viewport(0, 0, w, h)
+	gl.MatrixMode(lib.GL_PROJECTION)
+	gl.LoadIdentity()
+	gl.Ortho(0, 1, 1, 0, -100, 100)
+	gl.MatrixMode(lib.GL_MODELVIEW)
+	gl.LoadIdentity()
+
+	if draw and type(draw) == "function" then
+		local ok, err = pcall(draw, w, h)
+		if not ok then 
+			print(debug.traceback(err)) 
+			-- prevent error spew:
+			draw = nil
+		end	
+	end
+end
+win.oncreate = function(self) end
+win.onkey = function(self, e, k) 
+	e = key_events[e]
+	if k > 31 and k < 127 then
+		-- convert printable characters:
+		k = string.char(k)
+	end
+	if e == "down" then
+		-- built-in keys:
+		if k == 27 then
+			-- fullscreen flip:
+			self.fullscreen = not self.fullscreen
+		elseif k == " " then
+			-- pause/play:
+			updating = not updating
+		end
+		if keydown and type(keydown) == "function" then
+			local ok, err = pcall(keydown, k)
+			if not ok then print(debug.traceback(err)) end
+		end
+	elseif e == "up" then
+		if keyup and type(keyup) == "function" then
+			local ok, err = pcall(keyup, k)
+			if not ok then print(debug.traceback(err)) end
+		end
+	end
+	
+	if key and type(key) == "function" then
+		local ok, err = pcall(key, e, k)
+		if not ok then print(debug.traceback(err)) end
+	end
+end
+win.onmouse = function(self, e, b, x, y) 
+	if mouse and type(mouse) == "function" then
+		local ok, err = pcall(mouse, mouse_events[e], b, x / win.width, (self.height-y-1) / win.height)
+		if not ok then print(debug.traceback(err)) end
+	end
+end
+win.onvisible = function(self, s) end
+win.onresize = function(self, w, h) end
+
+return win
