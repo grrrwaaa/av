16348ff858325f468cdbb41d8b55133ec4d42807
grrrwaaa <grrrwaaa@gmail.com>
2013-08-15 15:01:59 -0700

    latest

--- allowarp.lua
+++ allowarp.lua
@@ -25,7 +25,7 @@ voxels:set(function(x, y, z)
 	local snx = nx*2-1
 	local sny = ny*2-1
 	local snz = nz*2-1
-	return math.sqrt(snx*snx + sny*sny + snz*snz)
+	return dim/2 * math.sqrt(snx*snx + sny*sny + snz*snz)
 end)
 
 ffi.cdef[[
